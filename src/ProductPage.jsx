bd30626f2a3d13891aa5bb3e05bdddbd6272b41c
Kofi Mensah <kofim10k@gmail.com>
2026-06-02 17:34:34 +0100

    fix cart

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useCart } from "./CartContext.jsx";
 
 const products = {
   "cap-black": {
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { CartProvider } from "./CartContext.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -8,23 +7,24 @@ import "./index.css";
 import App from "./App.jsx";
 import ProductPage from "./ProductPage.jsx";
 import StoryPage from "./StoryPage.jsx";
+import { CartProvider } from "./CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <BrowserRouter>
-  <audio
-    id="site-music"
-    src="/MP3Now.com_YouTube_GUNNA-WUNNA-COPYRIGHT-FREE-COPYRIGHT-FRE_Media_tT1_1RHA7Bo_007_128k.mp3"
-    loop
-  />
+    <BrowserRouter>
+      <audio
+        id="site-music"
+        src="/MP3Now.com_YouTube_GUNNA-WUNNA-COPYRIGHT-FREE-COPYRIGHT-FRE_Media_tT1_1RHA7Bo_007_128k.mp3"
+        loop
+      />
 
-  <CartProvider>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/products/:id" element={<ProductPage />} />
-      <Route path="/story" element={<StoryPage />} />
-    </Routes>
-  </CartProvider>
-</BrowserRouter>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/story" element={<StoryPage />} />
+        </Routes>
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
