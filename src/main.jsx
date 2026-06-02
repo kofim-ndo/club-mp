bfd36c15a18d3e291f8ed82f913be28ce2894deb
Kofi Mensah <kofim10k@gmail.com>
2026-06-02 17:22:39 +0100

    fix cart

--- src/main.jsx
+++ src/main.jsx
@@ -11,19 +11,20 @@ import StoryPage from "./StoryPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-   <audio id="site-music" src="/MP3Now.com_YouTube_GUNNA-WUNNA-COPYRIGHT-FREE-COPYRIGHT-FRE_Media_tT1_1RHA7Bo_007_128k.mp3" loop />
+   <BrowserRouter>
+  <audio
+    id="site-music"
+    src="/MP3Now.com_YouTube_GUNNA-WUNNA-COPYRIGHT-FREE-COPYRIGHT-FRE_Media_tT1_1RHA7Bo_007_128k.mp3"
+    loop
+  />
 
-   
-      <Routes>
-        <CartProvider>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/products/:id" element={<ProductPage />} />
-    <Route path="/story" element={<StoryPage />} />
-  </Routes>
-</CartProvider>
-      </Routes>
-    </BrowserRouter>
+  <CartProvider>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/products/:id" element={<ProductPage />} />
+      <Route path="/story" element={<StoryPage />} />
+    </Routes>
+  </CartProvider>
+</BrowserRouter>
   </React.StrictMode>
 );
